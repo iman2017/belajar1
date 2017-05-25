72a49816c3b242ad84d5f35a8e47cf09e27eb14a
Iman Muliawan <imanmuliawan@gmail.com>
2017-05-25 19:45:21 +0700

    pengiriman kedua

--- HitungLuas.java
+++ HitungLuas.java
@@ -9,15 +9,21 @@ public class HitungLuas {
 	int setengah = 1/2;
 	int alas = 5;
 	int tinggi = 8;
+        int phi = 22/7;
+        int jari2 = 5;
         int Luaspersegi = LuasPersegiPanjang(panjang, lebar);
         int Luassegitiga = LuasSegitiga(setengah, alas, tinggi);
-        System.out.println("Panjangnya "+panjang);
-        System.out.println("Lebarnya "+lebar);
+        int Luaslingkaran = cariLuasLingkaran(phi, jari2);
+        System.out.println("Panjangnya adalah "+panjang);
+        System.out.println("Lebarnya adalah   "+lebar);
         System.out.println("Luasnya Persegi Panjang adalah "+ Luaspersegi);
 	System.out.println("");
 	System.out.println("Panjang Alas adalah "+ alas);
 	System.out.println("Tinggi adalah       "+ tinggi);
 	System.out.println("Luasnya Segitiga adalah "+ Luassegitiga);
+        System.out.println("Phi adalah "+phi);
+        System.out.println("Jari - jari adalah "+jari2);
+        System.out.println("Luasnya Lingkaran adalah "+Luaslingkaran);
     }
 public static int LuasPersegiPanjang(int panjang, int lebar) {
         int luas;
@@ -28,5 +34,10 @@ public static int LuasSegitiga(int setengah, int alas, int tinggi)   {
 	int luas;
 	luas = alas*tinggi/2;
 	return luas;
-    } 
+    }
+public static int cariLuasLingkaran(int phi, int jari2) {
+        int luas;
+        luas = phi*(jari2*jari2);
+        return luas;
+    }
 }
